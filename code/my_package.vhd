c12001530d6ceb56f90e9830789bc0c2c06c206d
salonioswal <salonioswal98@gmail.com>
2018-08-20 09:29:36 +0200

    Add files via upload

--- code/my_package.vhd
+++ code/my_package.vhd
@@ -8,12 +8,14 @@ constant data_width: integer := 16; --input data widths
 CONSTANT n: INTEGER := 2; --Number of rows in the original image taken each time	
 constant m: integer:= n*4;
 constant ptr_width:integer:=integer(ceil(log2(real(n))));
-			
-   
+constant rows: integer:=3;			
+constant ptr_width_inv:integer:=integer(ceil(log2(real(rows)))); 
+
    type r_file is array (1 downto 0) of signed(data_width-1 downto 0);
    type m_file is array (1 downto 0) of r_file;
    type reg_file is array (n-1 downto 0) of m_file;
    type sram_coeff is array (m-1 downto 0) of signed(data_width-1 downto 0);
+	type regis_file is array (rows-1 downto 0) of signed(data_width-1 downto 0);
 end my_package; 
 
 
---
+++ code/result_x.vhd
@@ -0,0 +1,38 @@
+library ieee;
+library work;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+use work.my_package.all; 
+use work.all;
+
+entity reg_inv_x is
+	port(input_data	: in signed(data_width-1 downto 0);
+		 output_data: out signed(data_width-1 downto 0);
+		 clock		: in std_logic;
+		 reset		: in std_logic;
+		 rd_ptr		: in unsigned(ptr_width_inv-1 downto 0);
+		 wr_ptr		: in unsigned(ptr_width_inv-1 downto 0);
+		 wr_en		: std_logic);
+	end reg_inv_x;
+
+architecture beha_xreg of reg_inv_x is
+	signal register_file_x: regis_file;
+begin
+
+
+process(clock,reset)
+		output_data<=register_file(to_integer(rd_ptr));
+begin
+	
+	if (reset='0') then
+		register_file_x<=((others=>(others=>'0')));
+	elsif(rising_edge(clock)) then
+		if(wr_en='1') then
+		register_file_x(to_integer(wr_ptr))<=input_data;
+		end if;
+	end if;
+end process;
+	
+	
+end beha_xreg;
